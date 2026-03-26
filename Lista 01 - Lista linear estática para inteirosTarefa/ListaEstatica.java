528c637a33330e9acacc99e34dd6f5658e27137f
Amanda Eyng <141786591+AmandaEyng@users.noreply.github.com>
2026-03-26 00:18:52 -0300

    fix: funcao toString e retirar
    Com a ajuda da IA do Google fiz algumas alterações, visto que quando fui passar o plano de testes não estavam funcionando perfeitamente.

--- Lista 01 - Lista linear estática para inteirosTarefa/ListaEstatica.java
+++ Lista 01 - Lista linear estática para inteirosTarefa/ListaEstatica.java
@@ -1,105 +1,107 @@
-//inserir pacote
+package modelo;
 
 public class ListaEstatica {
 
-    private int[] info;
-    private int tamanho;
+	private int[] info;
+	private int tamanho;
 
-    public ListaEstatica() {
-        info = new int[10];
-        tamanho = 0;
-    }
+	public ListaEstatica() {
+		info = new int[10];
+		tamanho = 0;
+	}
 
-    private void redimencionar() {
+	private void redimencionar() {
 
-        int[] novo;
-        int novoTamanho = info.length + 10;
-        novo = new int[novoTamanho];
+		int[] novo;
+		int novoTamanho = info.length + 10;
+		novo = new int[novoTamanho];
 
-        for (int i = 0; i < info.length; i++) {
-            novo[i] = info[i];
-        }
+		for (int i = 0; i < info.length; i++) {
+			novo[i] = info[i];
+		}
 
-        info = novo;
-    }
+		info = novo;
+	}
 
-    public void inserir(int valor) {
+	public void inserir(int valor) {
 
-        if (tamanho == info.length) {
-            redimencionar();
-        }
+		if (tamanho == info.length) {
+			redimencionar();
+		}
 
-        info[tamanho] = valor;
-        tamanho = tamanho + 1;
-    }
+		info[tamanho] = valor;
+		tamanho = tamanho + 1;
+	}
 
-    public void exibir() {
+	public void exibir() {
 
-        for (int i = 0; i < tamanho; i++) {
-            System.out.println(info[i]);
-        }
+		for (int i = 0; i < tamanho; i++) {
+			System.out.println(info[i]);
+		}
 
-    }
+	}
 
-    public int buscar(int valor) {
-        int indice = -1;
+	public int buscar(int valor) {
+		int indice = -1;
 
-        for (int i = 0; i < tamanho; i++) {
-            if (valor == info[i]) {
-                indice = i;
-            }
-        }
+		for (int i = 0; i < tamanho; i++) {
+			if (valor == info[i]) {
+				indice = i;
+			}
+		}
 
-        return indice;
-    }
+		return indice;
+	}
 
-    public void retirar(int valor) {
-        int indiceRemover = buscar(valor);
+	public void retirar(int valor) {
+	    int indiceRemover = buscar(valor);
 
-        for (int i = indiceRemover; i < tamanho - 1; i++) {
-            info[i] = info[i + 1];
-        }
+	    // Adicione esta condição para só remover se encontrar o elemento
+	    if (indiceRemover != -1) {
+	        for (int i = indiceRemover; i < tamanho - 1; i++) {
+	            info[i] = info[i + 1];
+	        }
+	        tamanho--;
+	    }
+	}
 
-        tamanho--;
-    }
 
-    public void liberar() {
-        info = new int[10];
-        tamanho = 0;
-    }
+	public void liberar() {
+		info = new int[10];
+		tamanho = 0;
+	}
 
-    public int obterElemento(int posicao) {
-        if (posicao < 0 || posicao > tamanho) {
-            throw new IndexOutOfBoundsException("Posição " + posicao + " não existe");
-        }
+	public int obterElemento(int posicao) {
+		if (posicao < 0 || posicao >= tamanho) { // Correção: >= tamanho
+			throw new IndexOutOfBoundsException("Posição " + posicao + " não existe");
+		}
+		return info[posicao];
+	}
 
-        return info[posicao];
-    }
+	public boolean estaVazia() {
+		// if (tamanho == 0) {
+		// return true;
+		// }
+		// return false;
+		// o código acima pode ser resumido a linha que esta abaixo.
 
-    public boolean estaVazia() {
-        //if (tamanho == 0) {
-        //	return true;
-        //}
-        //return false;
-        // o código acima pode ser resumido a linha que esta abaixo.
+		return (tamanho == 0);
+	}
 
-        return (tamanho == 0);
-    }
+	public int getTamanho() {
+		return tamanho;
+	}
 
-    public int getTamanho() {
-        return tamanho;
-    }
-
-    public String toString() {
-        String s = "";
-        for (int i = 0; i < tamanho - 1; i++) {
-            s += info[i];
-            if (i < tamanho - 1) {
-                s += ", ";
-            }
-        }
-
-        return s;
-    }
+	public String toString() {
+	    String s = "";
+	    for (int i = 0; i < tamanho; i++) {
+	        s += info[i];
+	        // Só adiciona a vírgula e o espaço se NÃO for o último elemento
+	        if (i < tamanho - 1) {
+	            s += ", "; 
+	        }
+	    }
+	    return s;
+	}
 
 }
